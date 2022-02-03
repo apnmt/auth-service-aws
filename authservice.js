9d0d29950cd9493f13dbdf2610e8916461b18ad3
tobi5775 <tobias.wittmann96@web.de>
2022-02-03 13:06:02 +0100

    create authservice for aws lambda

---
+++ authservice.js
@@ -0,0 +1,51 @@
+const AWS = require('aws-sdk');
+
+// Initialize CognitoIdentityServiceProvider.
+const cognito = new AWS.CognitoIdentityServiceProvider({
+    apiVersion: "2016-04-18",
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY
+});
+
+const USERPOOLID = process.env.COGNITO_USER_POOL_ID;
+
+exports.handler = async (event, context) => {
+    const requestBody = JSON.parse(event.body);
+    console.log(requestBody);
+    const EMAIL = requestBody.email;
+    const USERNAME = requestBody.username;
+    const PASSWORD = requestBody.password;
+    const createUserParams = {
+        UserPoolId: USERPOOLID,
+        Username: USERNAME,
+        UserAttributes: [{
+            Name: "email",
+            Value: EMAIL,
+        },
+        {
+            Name: "email_verified",
+            Value: "true",
+        },
+        ],
+        TemporaryPassword: PASSWORD,
+    };
+
+    let createUserResponse = await cognito.adminCreateUser(createUserParams).promise();
+    console.log(JSON.stringify(createUserResponse, null, 2));
+
+    var setUserPasswordParams = {
+      Password: PASSWORD,
+      UserPoolId: USERPOOLID,
+      Username: USERNAME,
+      Permanent: true
+    };
+    let setUserPasswordResponseBody = await cognito.adminSetUserPassword(setUserPasswordParams).promise();
+    console.log(JSON.stringify(setUserPasswordResponseBody, null, 2));
+
+    const response = {
+        "statusCode": 200,
+        "body": JSON.stringify(createUserResponse),
+        "isBase64Encoded": false
+    };
+    return response
+}
